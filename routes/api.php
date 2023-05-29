273b3e6961d34b50398c719bc7ccc20b86ff839e
DavideAtzo <atzo9477@gmail.com>
2023-05-29 23:55:13 +0200

    rotte api technology

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TechnologyController;
 use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,5 @@ Route::get('projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
 Route::get('types', [TypeController::class, 'index']);
 Route::get('types/{slug}', [TypeController::class, 'show']);
+Route::get('technologies', [TechnologyController::class, 'index']);
+Route::get('technologies/{slug}', [TechnologyController::class, 'show']);

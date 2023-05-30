51286e49ea5e77e82f566e1580c8ccfa26ffc1be
DavideAtzo <atzo9477@gmail.com>
2023-05-30 18:42:02 +0200

    aggiunto validazione e salvataggio commento dal form

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function store(Request $request){
+    
+        $data = $request->validate([
+            'author' => 'required|string',
+            'content' => 'required|string',
+            'project_id' => 'integer|exists:projects,id'
+        ]);
+        $comment = new Comment();
+        $comment->author = $data['author'];
+        $comment->content = $data['content'];
+        $comment->project_id = $data['project_id'];
+        $comment->save();
+        return $comment;
+        return $request->all();
+    }
+}

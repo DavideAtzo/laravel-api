c7bd82d350aceb6e4cff39c9ecede9a23736651e
DavideAtzo <atzo9477@gmail.com>
2023-05-30 16:06:11 +0200

    seed faker per tabella comments

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,16 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 0; $i < 30; $i++) {
+
+            $project = Project::inRandomOrder()->first();
+
+            $comment = new Comment();
+            $comment->author = fake()->name;
+            $comment->content = fake()->text();
+            $comment->project_id = $project->id;
+            $comment->save();
+
+        }
     }
 }
